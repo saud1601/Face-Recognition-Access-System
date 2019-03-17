656e5dfb036826580749a9279c2adff60d6f9f95
Nasser-A <43550868+Nasser-A@users.noreply.github.com>
2019-03-18 00:45:33 +0300

    Add files via upload

---
+++ blynk.py
@@ -0,0 +1,32 @@
+import BlynkLib
+
+import RPi.GPIO as GPIO
+
+blynk = BlynkLib.Blynk('c2e97d056d744994b1b8073963dec4be' )
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.OUT)
+#sn = Blynk('00fc7672fafd447aad3ffe46d3bf89ea', pin= "V1") 
+blynk.notify('Door Online')
+
+@blynk.VIRTUAL_WRITE(1)
+
+def Door(value):
+      
+    if int(format(value[0])) == 1:
+        
+        GPIO.output(20, 1)
+        blynk.notify('Door Unlocked')
+
+        
+    else:
+        
+        
+        GPIO.output(20, 0)
+        blynk.notify('Door Locked')
+
+
+while True:
+    
+    blynk.run()
