d0a986ae91d60aeb02a87d36887666829830d1ce
Nasser-A <43550868+Nasser-A@users.noreply.github.com>
2019-03-18 00:24:09 +0300

    Update RegisterNewUser.py

--- RegisterNewUser.py
+++ RegisterNewUser.py
@@ -1,26 +1,42 @@
 import cv2
+import sys
+
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # W
-cam.set(4, 480) # H
+cam.set(3, 640) # set video width
+cam.set(4, 480) # set video height
 face_detector = cv2.CascadeClassifier('/home/pi/Desktop/Cascades/haarcascade_frontalface_default.xml')
-face_id = input('\nEnter user id then press enter:  ')
-print("\nInitializing face capture. Look the camera and wait ...")
+
+# Enter your name here..
+users = ['Rafed', 'Saud']
+    
+face_id = len(users)
+
+print ('\nYour ID is :' +str(face_id))
+        
+    
+print("\n Initializing face capture. Look the camera and wait ... (Press Ctrl+C to exit)")
+
 count = 0
+
 while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    try:
+        ret, img = cam.read()
+   
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    except KeyboardInterrupt:
+        print("\nExiting Program")
+        sys.exit()
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
-        # Save the captured image into the dataset folder
+        # Save the captured image into the datasets folder
         cv2.imwrite("/home/pi/Desktop/FaceRecognitionAccessSystem/dataset/" + str(face_id)  + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
-    k = cv2.waitKey(0)
-    if k == 27:
-        break
-    elif count >= 1:
+    
+    if count >= 1: 
          break
-print("\nExiting Program")
+
+print("\nUser created...Exiting Program")
 cam.release()
 cv2.destroyAllWindows()
