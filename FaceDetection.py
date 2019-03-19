aeadae66c92e61946e7796ce9d3ee8cee2d5485f
Nasser-A <43550868+Nasser-A@users.noreply.github.com>
2019-03-19 12:42:05 +0300

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -15,7 +15,7 @@ while True:
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-    cv2.imshow('video',img)
+    cv2.imshow('Detection',img)
     k = cv2.waitKey(0)
     if k == 27:
         break
